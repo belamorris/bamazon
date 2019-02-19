5fe5d1e7d73d6772b131c9077da62468cd3056c6
Robert M <belamorris@gmail.com>
2019-02-18 19:47:46 -0700

    conneted to the database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,23 @@
 // required dependencies
 var inquirer = require("inquirer");
 var mysql = require("mysql");
+
+// Mysql connection
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+
+    // username
+    user: "root",
+
+    //pasword and database
+    password: "",
+    database: "bamazon"
+});
+
+//check connection
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+   
+  });
